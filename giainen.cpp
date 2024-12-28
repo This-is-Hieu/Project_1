ed83a6852c68586b5989b2c149948a09bfd3a613
Hieu Nguyen <Hieu07062004@gmail.com>
2024-12-29 01:58:39 +0700

    Sua ham Generate, tao tester

--- giainen.cpp
+++ giainen.cpp
@@ -4,16 +4,20 @@
 
 using namespace std;
 
-deque<string> List;
-atomic<bool> Full = false;
-atomic<bool> Found = false;
-atomic<bool> Empty = true;
-queue<string> ErrPass;
-int day=0;
-int het = 0;
-mutex mtx;
+deque<string> List;                 //Deque chứa các mật khẩu để thử
+atomic<bool> Full = false;          //Khóa kiểm tra khi deque đầy
+atomic<bool> Found = false;         //Khóa kiểm tra khi tìm thấy kết quả
+atomic<bool> Empty = true;          //Khóa kiểm tra khi deque rỗng
+queue<string> ErrPass;              //Queue chứa số mật khẩu gây lỗi
+int day=0;                          //Biến đếm số lần deque đầy
+int het = 0;                        //Biến đếm số lần deque trống
+mutex mtx;                          //Khóa deque tránh xung đột
+vector<char> charList = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int length = 0;                     //Số kí tự trong mật khẩu
+const char *zip_filename = "D:/OneDrive/Desktop/123.zip";
+const char *file_name_in_zip = "123.txt";
 
-void Check() {
+void Check() {                      //Hàm kiểm tra trạng thái luồng
     while (true) {
         if(List.size() > 5000){
             Full = true;
@@ -33,30 +37,22 @@ void Check() {
     }
 }
 
-void Generate(){
-    char i,j,k,l;
-    string password = "0000";
-    for (i = 'a'; i <= 'z'; ++i) {
-        for (j = 'a'; j <= 'z'; ++j) {
-            for (k = 'a'; k <= 'z'; ++k) {
-                for (l = 'a'; l <= 'z'; ++l) {
-                    if(Full){
-                        this_thread::sleep_for(chrono::milliseconds(100));
-                    }
-                    else{
-                        password[0] = i;
-                        password[1] = j;
-                        password[2] = k;
-                        password[3] = l;
-                        mtx.lock();
-                        List.push_back(password);
-                        mtx.unlock();
-                    }
-                }
-            }
+
+void Generate(string &pass, int cur_length){
+    if (cur_length == length) {
+        while(List.size() > 5000) {
+            this_thread::sleep_for(chrono::milliseconds(100));
         }
+        mtx.lock();
+        List.push_back(pass);
+        mtx.unlock();
+        return;
     }
-    return;
+    for(char c : charList){
+        pass[cur_length] = c;
+        Generate(pass, cur_length +1);
+    }
+
 }
 
 void Test(const char *zip_filename, const char *file_name_in_zip) {
@@ -66,11 +62,11 @@ void Test(const char *zip_filename, const char *file_name_in_zip) {
         cout << "Khong the mo file zip  " << err << "\n";
         return;
     }
-    const char* password = "0000";
+    string pass ;
     while(!Found){
         mtx.lock();
-        if(!Empty){
-            password = List.front().c_str();
+        if(!List.empty()){
+            pass = List.front();
             List.pop_front();
             mtx.unlock();
         }
@@ -78,14 +74,14 @@ void Test(const char *zip_filename, const char *file_name_in_zip) {
             mtx.unlock();
             continue;
         }
-            cout << password <<"\n";
-            zip_file_t *file = zip_fopen_encrypted(archive, file_name_in_zip, 0, password);
+            cout << pass <<"\n";
+            zip_file_t *file = zip_fopen_encrypted(archive, file_name_in_zip, 0, pass.c_str());
             if(file){
-                ErrPass.push(password);
+                ErrPass.push(pass);
                 char buffer[1024];
                 int bytes_read = zip_fread(file, buffer, sizeof(buffer));
                 if (bytes_read > 0) {
-                    cout << "Mat khau dung la " << password << "\n";
+                    cout << "Mat khau dung la " << pass << "\n";
                     Found = true;
                     zip_fclose(file);
                     zip_close(archive);
@@ -97,21 +93,34 @@ void Test(const char *zip_filename, const char *file_name_in_zip) {
     zip_close(archive);
     return;
 }
+void input(int &num_thread) {
+    cout << "Nhap vao so luong" << endl;
+    cin >> num_thread;
+    cout << "Nhap vao so ky tu" << endl;
+    cin >> length;
+}
+
+void init() {
+
+}
+
 int main(){
-    const char *zip_filename = "D:/OneDrive/Desktop/123.zip";
-    const char *file_name_in_zip = "123.txt";
+    int num_thread = 4;
+    vector<thread> testers;
+    input(num_thread);
     thread checking(Check);
-    thread generator(Generate);
-    thread tester1(Test,zip_filename, file_name_in_zip);
-    thread tester2(Test,zip_filename, file_name_in_zip);
-    thread tester3(Test,zip_filename, file_name_in_zip);
-    thread tester4(Test,zip_filename, file_name_in_zip);
+    string pass(length, ' ');
+    thread generator(Generate, ref(pass), 0);
+    for(int i=0; i < num_thread; i++){
+        testers.push_back(thread(Test, zip_filename, file_name_in_zip));
+    }
     checking.join();
     generator.join();
-    tester1.join();
-    tester2.join();
-    tester3.join();
-    tester4.join();
+
+    for (auto &t : testers) {
+        t.join();
+    }
+
     while (!ErrPass.empty()) {
         cout << ErrPass.front() << endl;
         ErrPass.pop();
